c57e773cb83611e0c6405d6918ea26c61525d6de
Drew Conly <atconly@gmail.com>
2016-07-12 11:42:48 -0600

    finish loop practice

---
+++ loop.js
@@ -0,0 +1,42 @@
+var students = [{
+ name: 'Liz',
+ age: 25,
+ city: 'Boulder'
+},{
+ name: 'Meghan',
+ age: 27,
+ city: 'Denver'
+},{
+ name: 'Trent',
+ age: 32,
+ city: 'Boulder'
+},{
+ name: 'Chelsea',
+ age: 24,
+ city: 'Boulder'
+},{
+ name: 'Amir',
+ age: 18,
+ city: 'Denver'
+}];
+console.log('1)');
+for (i=0; i<students.length; i++) {
+  console.log(students[i].age);
+}
+console.log('2)');
+for (i=0; i<students.length; i++) {
+  console.log(students[i].name);
+  console.log(students[i].city);
+}
+console.log('3)');
+for (i=0; i<students.length; i++) {
+  if (students[i].city == 'Boulder') {
+    console.log(students[i].name + ' is from ' + students[i].city);
+  }
+}
+console.log('4)');
+for (i=0; i<students.length; i++) {
+  if (students[i].age > 25) {
+    console.log(students[i].name + ' is older than 25');
+  }
+}
